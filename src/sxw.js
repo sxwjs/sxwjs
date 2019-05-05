6daca5a7b64ff346ea6b3720144de96ebd33cd60
Kolappan Nathan <kolappannathan@users.noreply.github.com>
2019-05-05 19:11:16 +0530

    Plain XSS warning text

---
+++ src/sxw.js
@@ -0,0 +1,6 @@
+function printXssWarning() {
+    console.log("*********  *********  *********  *********\n***           ***     ***   ***  ***   ***\n***           ***     ***   ***  ***   ***\n*********     ***     ***   ***  *********\n      ***     ***     ***   ***  ***\n      ***     ***     ***   ***  ***\n*********     ***     *********  ***");
+    console.log("\nDO NOT PROCEED.\n\nThis section is intended for developers only. Don't copy paste anything in this area.")
+}
+
+printXssWarning();

a394558df6cdbb9357da1c5d7491e6c780995145
Kolappan Nathan <kolappannathan@users.noreply.github.com>
2019-05-10 08:31:06 +0530

    Fixes #1, #2

--- src/sxw.js
+++ src/sxw.js
@@ -1,7 +1,7 @@
 window.sxwjs = {};
 window.sxwjs.printXssWarning = function printXssWarning() {
-    console.log("*********  *********  *********  *********\n***           ***     ***   ***  ***   ***\n***           ***     ***   ***  ***   ***\n*********     ***     ***   ***  *********\n      ***     ***     ***   ***  ***\n      ***     ***     ***   ***  ***\n*********     ***     *********  ***");
-    console.log("DO NOT PROCEED.\n\nThis section is intended for developers only. Don't copy paste anything in this area.")
+    console.log("%c*********  *********  *********  *********\n***           ***     ***   ***  ***   ***\n***           ***     ***   ***  ***   ***\n*********     ***     ***   ***  *********\n      ***     ***     ***   ***  ***\n      ***     ***     ***   ***  ***\n*********     ***     *********  ***", "color: red; font-weight:bold;");
+    console.log("%cDO NOT PROCEED.", "font-weight:bold; font-size:15px;", "\n\nThis section is intended for developers only. Don't copy paste anything in this area.\nIf someone told you to copy and paste something here, it is a scam and will give them access to your account. In that case, kindly report this to our support tea.");
 }
 
 window.sxwjs.printXssWarning();
